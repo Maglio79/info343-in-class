e61d5e2dbaaa13e25e38a6513dd2263de9a1ffd7
Maglio79 <chris.maglio79@gmail.com>
2016-11-03 10:32:42 -0700

    idk man

--- firebase/js/app.js
+++ firebase/js/app.js
@@ -13,14 +13,14 @@
 
 //paste the Firebase initialization code here
 // Initialize Firebase
-var config = {
+  var config = {
     apiKey: "AIzaSyBOhMGEU-pocVlVxojvCQBQjZmdBZoJFPw",
     authDomain: "tasks-demo-43aff.firebaseapp.com",
     databaseURL: "https://tasks-demo-43aff.firebaseio.com",
     storageBucket: "tasks-demo-43aff.appspot.com",
     messagingSenderId: "520086663439"
-};
-firebase.initializeApp(config); 
+  };
+  firebase.initializeApp(config);
 
 var currentUser;
 var authProvider = new firebase.auth.GithubAuthProvider();
